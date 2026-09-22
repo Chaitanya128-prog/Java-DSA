c5e27d0dd7d4f1f9a2782beed50994b4d9c65418
Chaitanya Pandey <pandeychintu07@gmail.com>
2026-09-22 23:22:15 +0530

    Add Strings DSA practice

---
+++ src/dsa/Strings/StringsBasics.java
@@ -0,0 +1,10 @@
+package dsa.Strings;
+
+public class StringsBasics {
+    public static void main(String[] args) {
+        char[] arr = {'c','h','a','i','t','a','n','y','a'};
+        for(char ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
